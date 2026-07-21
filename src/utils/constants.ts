48601f69ebc4f9c2ca897f3088aeab3430ca6744
MatiDZ <90805957+MatiDeZeta@users.noreply.github.com>
2026-07-21 20:01:24 -0300

    Fix CUSTOM_EMOJI format in constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const EMOJI = "🍯";
 export const CUSTOM_EMOJI_ID = "1450060724943720600";
-export const CUSTOM_EMOJI = `<:honeypot:${CUSTOM_EMOJI_ID}>`;
+export const CUSTOM_EMOJI = `<:honey_pot:${CUSTOM_EMOJI_ID}>`;
 
 export const HAS_MESSAGE_INTENT = process.env.HAS_MESSAGE_INTENT === "true" || process.env.HAS_MESSAGE_INTENT === "1";
